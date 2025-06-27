d52cb6910123143724790e8087c03602c517775e
@amal7784404 <amal@trackerhero.com>
2025-06-27 23:47:00 +0800

    cleanup useEffect

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,24 +7,38 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Could not fetch the data for that resource");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
-      }, 1000);
+    // setIsPending(true);
+    // setData(null);
+
+    const controller = new AbortController(); // For cleanup
+
+    setTimeout(() => {
+      fetch(url, { signal: controller.signal })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name !== "AbortError") {
+            setError(err.message);
+            setIsPending(false);
+          }
+        });
+    }, 500); // 1-second simulated delay
+
+    // Cleanup function if component unmounts before fetch finishes
+    return () => controller.abort();
   }, [url]);
+
   return { data, isPending, error };
 };
 
