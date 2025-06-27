c06665a92d11d5540013a03ba46ff397cc5dfea3
@amal7784404 <amal@trackerhero.com>
2025-06-27 23:04:45 +0800

    Custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,20 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 //useEffect = re-render tapi better guna untuk certain data for render kat DOM (use dependencies [])
 //useEffect good for fetch data
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => setBlogs(data))
-      .catch((error) => console.error("Fetch error:", error));
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} />}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       {!blogs && !isPending && <div>No blogs to display</div>}
     </div>
   );
